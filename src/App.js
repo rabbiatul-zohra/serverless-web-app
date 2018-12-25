6a62b4ee84a548eeea27cfdde9ef26c06a55f423
Rabbia Tul-Zohra <rabbia.tul-zohra@bbc.co.uk>
2018-12-25 20:01:04 +0000

    adds view to add items on screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
 import Amplify, { API } from "aws-amplify";
 import aws_exports from "./aws-exports";
+
+import ItemDashboard from "./screens/itemDashboard";
+import { Segment, Menu, Icon } from "semantic-ui-react";
 
 Amplify.configure(aws_exports);
 
@@ -17,22 +20,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Segment>
+        <Menu>
+          <Menu.Item name="home">
+            <Icon name="shop" />
+          </Menu.Item>
+          <Menu.Item name="Items" />
+          <Menu.Item name="aboutUs" />
+        </Menu>
+        <ItemDashboard />
+      </Segment>
     );
   }
 }
---
+++ src/screens/createItem.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+import { Form, Modal, Button } from "semantic-ui-react";
+
+import { API } from "aws-amplify";
+
+class CreateItemModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { itemName: "", itemPrice: "", itemDescription: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event, { name, value }) {
+    this.setState({ [name]: value });
+  }
+  handleSubmit(event) {
+    let apiName = "sampleCloudApi";
+    let path = "/items";
+    let newItem = {
+      body: {
+        name: this.state.itemName,
+        price: this.state.itemPrice,
+        description: this.state.itemDescription
+      }
+    };
+    API.post(apiName, path, newItem)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+    event.preventDefault();
+    this.handleClose();
+  }
+  handleOpen = () => this.setState({ modalOpen: true });
+  handleClose = () => this.setState({ modalOpen: false });
+
+  render() {
+    return (
+      <Modal
+        trigger={<Button onClick={this.handleOpen}>+ Add Item</Button>}
+        closeIcon={true}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+      >
+        <Modal.Header>Add an Item</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group unstackable widths={2}>
+              <Form.Input
+                name="itemName"
+                label="Item Name"
+                placeholder="Enter Item Name..."
+                onChange={this.handleChange}
+                value={this.state.itemName}
+              />
+              <Form.Input
+                name="itemPrice"
+                label="Item Price"
+                placeholder="£0.00"
+                onChange={this.handleChange}
+                value={this.state.itemPrice}
+              />
+            </Form.Group>
+            <Form.TextArea
+              name="itemDescription"
+              label="Item    Description"
+              placeholder="Add a Description of the Item..."
+              onChange={this.handleChange}
+              value={this.state.itemDescription}
+            />
+            <Form.Button type="submit">Submit</Form.Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
+export default CreateItemModal;
